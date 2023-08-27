3a95038c966dc4d96db8d3d5d48b15caa69ba589
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-27 16:10:54 +0300

    Modified react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-
 import './App.css';
+import React from 'react';
+import { Brand, CTA, NavBar} from './Components'
+import {Footer, Header, Blog, Possibility, What, Features} from './Containers'
 
 function App() {
-  return 
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <NavBar />
+        <Header />
+      </div>
+      <Brand />
+      <What />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+
+    </div>
+  )
 }
 
 export default App;
